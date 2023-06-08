9645339fce001e8e4d8663aea178a3dc56a44a59
Yana Antipova <antipovayana01@gmail.com>
2023-06-08 15:04:06 +0300

    Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int NumberA = 3;
+﻿int NumberA = new Random().Next(22, 88); // 22 ... 87
+Console.WriteLine(NumberA);
 int NumberB = 5;
+Console.WriteLine(NumberB);
 int result = NumberA + NumberB;
 Console.WriteLine (result);
 
