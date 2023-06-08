f0e5af878be7696a2d3d7b534617e7e44a75fe44
Yana Antipova <antipovayana01@gmail.com>
2023-06-08 15:26:47 +0300

    Example005_If-Else

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿string username = Console.ReadLine ();
+
+if(username.ToLower() == "яна")
+{
+    Console.WriteLine("Ура, это же Яночка!!!");
+}
+else
+{
+    Console.WriteLine("Здравствуй, ");
+    Console.Write(username);
+}
