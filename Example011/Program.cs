2014cf8f9f60b7936e05bb5842908e8225ea7fee
Yana Antipova <antipovayana01@gmail.com>
2023-06-13 11:08:57 +0300

    Создаем рандомный массив и ищем в нем значение

---
+++ Example011/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+
+
+}
+
+
+void PrintArray (int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array = new int [10]; //создай новый массив, которы будет наполнен 10 значениями
+                             //по-умолчанию нулями
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+int pos = IndexOf(array, 554);
+Console.WriteLine(pos);
